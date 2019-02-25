f53f2ac668d35ac65efee95764cea254040f091d
samoreal <31625943+samoreal@users.noreply.github.com>
2019-02-24 23:26:28 -0500

    BungieApi class can now get manifest, check if the current asset db is old, and download latest asset db

--- app/bungieapi.py
+++ app/bungieapi.py
@@ -1,4 +1,4 @@
-import json, requests, zipfile, io, sqlite3, os
+import json, requests, os, glob, re
 
 class BungieApi:
     base_url = 'https://www.bungie.net'
@@ -37,6 +37,9 @@ class BungieApi:
         if self.api_key is None:
             raise Exception('API key not set for this application')
 
+    ############################################################################
+    # LOGIN STUFF
+    ############################################################################
     def authorize(self, code):
         params = {
             'grant_type' : 'authorization_code',
@@ -49,3 +52,105 @@ class BungieApi:
 
         resp = requests.post(self.token_url, data=params)
         return json.loads(resp.text)
+
+    def refresh(self, refresh_token):
+        params = {
+            'grant_type' : 'refresh_token',
+            'client_id' : self.client_id,
+            'client_secret' : self.client_secret,
+            'refresh_token' : refresh_token
+        }
+
+        print ("Refreshing using refresh: {}".format(refresh_token))
+
+        resp = requests.post(self.token_url, data=params)
+        return json.loads(resp.text)
+
+    ############################################################################
+    # MANIFEST AND ASSET DATABASE STUFF
+    ############################################################################
+    def _getLatestManifestFilename(self):
+        manifests = glob.glob(os.path.join('.', 'assets', 'manifest_*.json'))
+        if len(manifests) == 0:
+            return None
+        manifests.sort(key=os.path.getmtime, reverse=True)
+        return manifests[0]
+
+    def _collapseVersion(self, version):
+        return re.sub('\D', "", version)
+
+    def getManifest(self):
+        resp = requests.get(
+            self.platform_url + '/Destiny2/Manifest/',
+            headers = { 'X-API-Key' : self.api_key }
+        )
+        return resp.json()
+
+    def shouldUpdateAssetDatabase(self, manifest):
+        currentVersion = self._collapseVersion(manifest['Response']['version'])
+        latestManifest = self._getLatestManifestFilename()
+
+        if latestManifest is None:
+            return True
+
+        try:
+            versionRe = 'manifest_([^.]+)\.json$'
+            cachedManifestVersion = re.search(versionRe, latestManifest]).group(1)
+            return currentVersion > cachedManifestVersion
+        except AttributeError:
+            print("Malformed manifest file name: {}".format(latestManifest))
+            return True
+
+    def getAssetDatabase(self, manifest):
+        resp = requests.get(
+            self.base_url + manifest['Response']['jsonWorldContentPaths']['en'],
+            headers={ 'X-API-Key' : self.api_key }
+        )
+
+        version = self._collapseVersion(manifest['Response']['version'])
+        filename = "manifest_{}.json".format(version)
+        with open(os.path.join(".", "assets", filename), 'wb') as file:
+            file.write(resp.content)
+
+    ############################################################################
+    # CHARACTER STUFF
+    ############################################################################
+    def getMemberships(self, access_token):
+        headers = {
+            'Authorization' : 'Bearer ' + access_token,
+            'X-API-Key' : self.api_key
+        }
+
+        r = requests.get(
+            base+'/User/GetMembershipsForCurrentUser/',
+            headers=headers
+        )
+        return json.loads(r.text)
+
+    def getCharacters(self, access_token, membership_id):
+        headers = {
+            'Authorization' : 'Bearer ' + access_token,
+            'X-API-Key' : self.api_key
+        }
+        params = { 'components' : ['100', '200'] }
+
+        r = requests.get(
+            base+'/Destiny2/2/Profile/'+membership_id+'/',
+            params=params,
+            headers=headers
+        )
+        return json.loads(r.text)
+
+    def getCharacterItems(self, access_token, membership_id, character_id):
+        headers = {
+            'Authorization' : 'Bearer ' + access_token,
+            'X-API-Key' : self.api_key
+        }
+        params = { 'components' : ['201', '205'] }
+
+        r = requests.get(
+            base+'/Destiny2/2/Profile/'+membership_id+'/Character/'+character_id+'/',
+            params=params,
+            headers=headers
+        )
+        return json.loads(r.text)
--- app/sweeperbot.py
+++ app/sweeperbot.py
@@ -1,64 +1,18 @@
 import json, requests, zipfile, io, sqlite3
 from inventoryitem import Inventory, InventoryItem
+from bungieapi import BungieApi
 
-code = ''
-access_token = ''
-refresh_token = ''
-
-bungie_id = '10253057'
-ps_membership_id = '4611686018448173014'
-bliz_membership_id = '4611686018468068743'
-titan_char_id = '2305843009261191525'
-
-headers = {
-    'Authorization' : 'Bearer ' + access_token,
-    'X-API-Key' : api_key
-}
-
-def refresh():
-    params = {
-        'grant_type' : 'refresh_token',
-        'client_id' : client_id,
-        'client_secret' : client_secret,
-        'refresh_token' : refresh_token
-    }
-
-    r = requests.post(token_endpoint, data=params)
-    print (json.dumps(json.loads(r.text), indent=2))
-
-def getManifest():
-    r = requests.get(base+'/Destiny2/Manifest/', headers={'X-API-Key' : api_key})
-    if r.ok:
-        manifest = r.json()
-        version = manifest['Response']['version']
-        assets = manifest['Response']['mobileWorldContentPaths']['en']
-
-        r = requests.get('https://www.bungie.net'+assets, headers={'X-API-Key' : api_key})
-        if r.ok:
-            zip = zipfile.ZipFile(io.BytesIO(r.content))
-            zip.extractall('./assets/manifest.db')
-        else:
-            print(r.request.headers)
-            print(r.request.url)
-    else:
-        print (r.text)
-
-def getMemberships():
-    r = requests.get(base+'/User/GetMembershipsForCurrentUser/', headers=headers)
-    print (json.dumps(json.loads(r.text), indent=2))
-
-def getCharacters():
-    params = { 'components' : ['100', '200'] }
-    r = requests.get(base+'/Destiny2/2/Profile/'+ps_membership_id+'/', params=params, headers=headers)
-    print (json.dumps(json.loads(r.text), indent=2))
-
-def getCharacterItems():
-    params = { 'components' : ['201', '205'] }
-    r = requests.get(base+'/Destiny2/2/Profile/'+ps_membership_id+'/Character/'+titan_char_id+'/',
-        params=params, headers=headers)
-    print (json.dumps(json.loads(r.text), indent=2))
+if __name__ == '__main__':
+    main()
 
 def main():
+    bapi = BungieApi()
+    manifest = bapi.getManifest()
+    if (bapi.shouldUpdateAssetDatabase(manifest)):
+        bapi.getAssetDatabase(manifest)
+
+
+def old_main():
     manifest = None
 
     with open('helmet2.json') as f:
@@ -97,6 +51,3 @@ def main():
     for perk in perklist.values():
         if perk not in inventory.perkmap:
             print(perk + " not on any inventory item!")
-
-if __name__ == '__main__':
-    getManifest()
